957ab9e6f9cd5228d374b778cb82e11fa70abd73
Mark Egesdal <megesdal@gmail.com>
2016-03-19 11:26:09 -1000

    Flip from map to 'flip map' operator. Reads better?

--- src/BinomialHeap.purs
+++ src/BinomialHeap.purs
@@ -1,7 +1,7 @@
 module BinomialHeap (BinomialHeap) where
 
 
-import Prelude (class Ord, (+), (<), (<=), (<$>))
+import Prelude (class Ord, (+), (<), (<=), (<#>))
 import Data.List (List(..), (:), null, reverse)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..), fst)
@@ -15,20 +15,20 @@ newtype BinomialHeap a = BH (List (BinomialTree a))
 data BinomialTree a = Node Int a (List (BinomialTree a))
 
 
-link :: forall a. Ord a => BinomialTree a -> BinomialTree a -> BinomialTree a
-link t1@(Node r x1 c1) t2@(Node _ x2 c2) =
-  if x1 <= x2 then
-    Node (r + 1) x1 (t2 : c1)
-  else
-    Node (r + 1) x2 (t1 : c2)
-
-
 rank :: forall a. BinomialTree a -> Int
 rank (Node r _ _) = r
 
 
 root :: forall a. BinomialTree a -> a
 root (Node _ x _) = x
+
+
+link :: forall a. Ord a => BinomialTree a -> BinomialTree a -> BinomialTree a
+link t1@(Node r x1 c1) t2@(Node _ x2 c2) =
+  if x1 <= x2 then
+    Node (r + 1) x1 (t2 : c1)
+  else
+    Node (r + 1) x2 (t1 : c2)
 
 
 insTree :: forall a. Ord a => BinomialTree a -> List (BinomialTree a) -> List (BinomialTree a)
@@ -63,7 +63,7 @@ removeMinTree (Cons t ts) =
       else
         Tuple t' (t : ts')
   in
-    takeMin <$> removeMinTree ts
+    removeMinTree ts <#> takeMin
 
 
 instance binomialHeap :: Heap BinomialHeap where
@@ -74,12 +74,11 @@ instance binomialHeap :: Heap BinomialHeap where
   insert x (BH ts) = BH (insTree (Node 0 x Nil) ts)
   merge (BH ts1) (BH ts2) = BH (mrg ts1 ts2)
 
-  findMin (BH ts) =
-    root <$> fst <$> removeMinTree ts
-
+  -- TODO: make findMin O(1)
+  findMin (BH ts) = removeMinTree ts <#> fst <#> root
   deleteMin (BH ts) =
     let
       addBackChildren (Tuple (Node _ _ ts1) ts2) =
         BH (mrg (reverse ts1) ts2)
     in
-      addBackChildren <$> removeMinTree ts
+      removeMinTree ts <#> addBackChildren
